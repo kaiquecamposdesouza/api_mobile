dfa28af16e7305307d456bc3a1d80edc949b79bc
kaique <kaique.souza@aluno.faculdadeimpacta.com.br>
2021-06-02 21:16:24 -0300

    api mobile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, json
 from BD import Seleciona
+import os
 
 selec = Seleciona()
 
@@ -67,4 +68,6 @@ def agenda():
 
     return ler_json_agenda
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='127.0.0.1', port=port, debug=True)
